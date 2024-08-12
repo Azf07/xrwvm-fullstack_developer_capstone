12bcad8f9727ee247d019de0eb1c88eaa831540f
Azf07 <fahmi-azerar@outlook.com>
2024-08-12 16:15:52 -0400

    User Management

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,25 @@
-import LoginPanel from "./components/Login/Login"
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Dealers from "./components/Dealers/Dealers";
 
 function App() {
   return (
     <Routes>
+      {/* Route for the login page */}
       <Route path="/login" element={<LoginPanel />} />
+
+      {/* Route for the registration page */}
+      <Route path="/register" element={<Register />} />
+
+      {/* Route for the dealers page */}
+      <Route path="/dealers" element={<Dealers />} />
+
+      {/* You can add more routes here for other components */}
     </Routes>
   );
 }
+
 export default App;
+
