3e3b57c8c3976b48870beb081ca987d14428930e
Azf07 <fahmi-azerar@outlook.com>
2024-08-13 02:49:21 +0200

    Fix PEP 8 issues and code cleanup

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
+
 # CarModelInline class
 class CarModelInline(admin.TabularInline):
     model = CarModel
     extra = 1
+
 
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
@@ -12,12 +14,14 @@ class CarMakeAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     search_fields = ['name']
 
+
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'dealer_id', 'car_make', 'type', 'year')
     list_filter = ['type', 'car_make', 'year']
     search_fields = ['name', 'car_make__name']
 
+
 # Register models here
-admin.site.register(CarMake)
-admin.site.register(CarModel)
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -23,14 +23,22 @@ urlpatterns = [
 
     # Path for getting dealerships
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-        path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(
+        route='get_dealers/<str:state>',
+        view=views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
 
     # Path for getting dealer details
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
     # Path for getting reviews of a specific dealer by ID
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_reviews_by_id'),
+    path(
+        route='reviews/dealer/<int:dealer_id>',
+        view=views.get_dealer_reviews,
+        name='dealer_reviews_by_id'
+    ),
 
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
